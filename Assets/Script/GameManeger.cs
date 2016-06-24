0eb5c26eedcc12dd94acc4d5345f280014ab6f4a
桝本彩乃 <dg.lw.8aya4@gmail.com>
2016-06-24 19:09:16 +0900

    6/24 19:00現在

---
+++ Assets/Script/GameManeger.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameManeger : MonoBehaviour {
+
+	public Image gauge;
+	public float power;
+	public int round;
+	public float maxpower;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		power = 0;
+		round = 1;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Touch tch = Input.GetTouch(0);
+
+		//round=1の時maxpower=20;
+		if (round == 1) {
+			maxpower = 20;
+		}
+
+		//"drink"というタグのスプライトを１秒撫でたら元気パワーが１上がる
+
+		//if(Input.touchCount == 1){
+
+
+	//}
+
+		//Touchという関数を作る
+//		Touches ();
+
+		//とりあえず１秒たったら１上がる
+		//power += 1 * Time.deltaTime;
+
+		//gauge.fillAmount = (現在の元気パワー）/ (MAXの元気パワー);と書く
+		gauge.fillAmount = power / maxpower;
+		Debug.Log (power);
+	
+	}
+
+//	void Touches(){
+//		if(Input.GetMouseButton(0)){
+//			if (Input.mousePosition == GameObject.FindWithTag ("drink").transform.position) {
+//
+//					power += 1 * Time.deltaTime;
+//
+//
+//			}
+//		}
+//
+//	
+//	}
+
+//	void OnCollisionStay2D(Collision2D coll){
+//		if (coll.gameObject.tag == "drink") {
+//			
+//			power += 1 * Time.deltaTime;
+//
+//		}
+//
+//	}
+
+		
+}
---
+++ Assets/Script/mouse.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mouse : MonoBehaviour {
+
+	private Vector3 screenPoint;
+	private Vector3 offset;
+	GameManeger script;
+
+
+	// Use this for initialization
+	void Start () {
+
+		script = GetComponent<GameManeger> ();
+		//script.power;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnMouseDown() {
+		this.screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+		this.offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+		//ポジションとクリックした場所のズレを計算
+	}
+
+	void OnMouseDrag() {
+		Vector3 currentScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+		Vector3 currentPosition = Camera.main.ScreenToWorldPoint(currentScreenPoint) + this.offset; //ズレをたす
+		transform.position = currentPosition;
+	}
+
+//	void OnCollisionStay2D(Collision2D coll){
+//		if (coll.gameObject.tag == "drink") {
+//
+//			power = GetComponent<GameManeger> ();
+//			power += 1 * Time.deltaTime;
+//
+//		}
+//
+//	}
+
+}
